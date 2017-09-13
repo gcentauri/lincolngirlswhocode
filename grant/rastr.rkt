21c518b0bfe9aa4649b8306b914e731e735e3b93
Grant Shangreaux <gcentauri@gmail.com>
2017-09-13 17:58:24 -0500

    updated asteroid demo

--- grant/rastr.rkt
+++ grant/rastr.rkt
@@ -3,7 +3,7 @@
 
 (define-struct game [canvas ship])
 
-(define-struct ship [location velocity])
+(define-struct ship [location velocity angle])
 
 (define-struct posn [x y])
 
@@ -13,7 +13,22 @@
   (make-posn (+ (posn-x posn) (vel-deltax velocity))
              (+ (posn-y posn) (vel-deltay velocity))))
 
-(define player-ship (make-ship (make-posn 250 400) (make-vel 0 0)))
+(define avatar (add-line (triangle 30 'outline 'white)
+                         15 0 15 15 'red))
+
+(define player-ship (make-ship (make-posn 250 250)
+                               (make-vel 0 0)
+                               0))
+
+(define (ship-on-blank the-hero)
+  (define the-hero avatar)
+  (define w (image-width the-hero))
+  (define h (image-height the-hero))
+  (define d (max w h))
+  (define dx (/ w 2))   ; centroid x offset
+  (define dy (* 2/3 h)) ; centroid y offset
+  (define blank  (circle d "solid" "black"))
+  (place-image/align the-hero (- d dx) (- d dy) "left" "top" blank))
 
 (define (ship-x s)
   (posn-x (ship-location s)))
@@ -22,36 +37,57 @@
   (posn-y (ship-location s)))
 
 (define (draw-ship s)
-  (triangle 20 'outline 'white))
+  (rotate (ship-angle s) (ship-on-blank avatar)))
+
+(define (degrees->radians d)
+  (* d (/ pi 180)))
 
 (define (accelerate-ship s)
+  (define move-velocity
+    (make-vel (* (sin (degrees->radians (ship-angle s))) -1)
+              (* (cos (degrees->radians (ship-angle s))) -1)))
   (make-ship (ship-location s)
-             (make-vel (vel-deltax (ship-velocity s))
-                       (- (vel-deltay (ship-velocity s)) 0.3))))
+             (make-vel (+ (vel-deltax (ship-velocity s))
+                          (vel-deltax move-velocity))
+                       (+ (vel-deltay (ship-velocity s))
+                          (vel-deltay move-velocity)))
+             (ship-angle s)))
+
+(define (turn-ship a s)
+  (make-ship (ship-location s)
+             (ship-velocity s)
+             (+ (ship-angle s) a)))
 
 (define (move-ship s)
   (make-ship (add-velocity (ship-location s) (ship-velocity s))
-             (ship-velocity s)))
-
-(define (drive-ship s)
-  (move-ship (accelerate-ship s)))
+             (ship-velocity s)
+             (ship-angle s)))
 
 (define background (overlay (square 500 'solid 'black)
                             (empty-scene 500 500)))
 
+(define (put-image i p s)
+  (place-image i (posn-x p) (posn-y p) s))
 
 (define rastr (make-game background player-ship))
 
 (define (tick-rastr g)
-  (make-game background (drive-ship (game-ship g))))
+  (make-game background (move-ship (game-ship g))))
 
 (define (draw-rastr g)
-  (place-image (draw-ship (game-ship g))
-               (ship-x (game-ship g))
-               (ship-y (game-ship g))
-               (game-canvas g)))
+  (let ([ship (game-ship g)])
+    (put-image (draw-ship ship)
+               (ship-location ship)
+               (game-canvas g))))
+
+(define (change g a-key)
+  (cond
+    [(key=? a-key "left") (make-game background (turn-ship 15 (game-ship g)))]
+    [(key=? a-key "right") (make-game background (turn-ship -15 (game-ship g)))]
+    [(key=? a-key "up") (make-game background (accelerate-ship (game-ship g)))]
+    ))
 
 (big-bang rastr
+  [on-key change]
   [to-draw draw-rastr]
   [on-tick tick-rastr])
-
